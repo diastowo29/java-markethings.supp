ff0f9d486eed668411a9db220cb5650f9f687588
diastowo29 <diastowo@gmail.com>
2018-04-04 10:56:14 +0700

    change port

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		System.getProperties().put("server.port", 5000);
+//		System.getProperties().put("server.port", 5000);
 //		System.out.println("testing");
 		SpringApplication.run(Application.class, args);
 	}
